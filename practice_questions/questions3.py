c3481937cc95542718113599515d93aa57025998
Yashshakya1 <yashshakya787@gmail.com>
2025-05-04 18:59:58 +0530

    some update in programe

--- practice_questions/questions3.py
+++ practice_questions/questions3.py
@@ -1,8 +1,8 @@
 # sum a two numers 
 # or also find the remainder and quiescent 
 
-a = 54
-b = 50
+a = int(input("enter your number 1:-"))
+b = int(input("enter your number 2:-"))
 print(a+b)
 
 print("this is a remainder ;-",a%b)

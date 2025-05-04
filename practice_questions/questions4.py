43944f7d4d42817d52f4e03c7c51105dcbd86051
Yashshakya1 <yashshakya787@gmail.com>
2025-05-04 19:10:41 +0530

    find average of two numbers

---
+++ practice_questions/questions4.py
@@ -0,0 +1,4 @@
+# find the average of two number enter value by the user
+num1 = int(input("enter the num first :-"))
+num2 = int(input("enter tne num secand :-"))
+print("this is the average of two numbers :-",num1+num2/2)

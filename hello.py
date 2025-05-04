e8213fb977e9898f99909b3dab869cb17f719ffb
Yashshakya1 <yashshakya787@gmail.com>
2025-05-04 17:11:13 +0530

    typecasting

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
-print("hello");
-# name = "yash";
-number1 = input("enter your number:-");
-number2 = input("enter your number:-");
-sum = (number1 + number2);
+print("hello")
+# name = "yash"
+number1 = input("enter your number 1")
+number2 = input("enter your number 2")
+sum = (number1 + number2)
 
-print(sum);
+print(sum)
---
+++ practice_questions/questions2.py
@@ -0,0 +1,5 @@
+# Q2. typecasting 
+
+Mathes = 87.555
+result = type(Mathes)
+print(result)

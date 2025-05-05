def27dfc7dc54e643c4b98e79ed01647bae0cc07
Yashshakya1 <yashshakya787@gmail.com>
2025-05-05 22:42:50 +0530

    new concept list

---
+++ list.py
@@ -0,0 +1,13 @@
+# list is a container to store the set of values of a data types
+# list is a mutable
+
+cars = ["limbo","aura","honda","ev cars","tvs"]
+print( "types of cars and bike in india :-",cars)
+anythink = ["sadfsa",48,484.54,False]
+print(anythink[0])
+anythink[1] = "yash"
+print(anythink[1])
+# list also used as a string slice for ex:-
+
+print("here it is a sliceing in the list :-",cars[:3])
+print("here it is a sliceing in the list :-",cars[-5:-1])

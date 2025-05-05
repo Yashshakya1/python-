72a4675141799e599d060c6f269c6a46670cb211
Yashshakya1 <yashshakya787@gmail.com>
2025-05-05 22:16:20 +0530

    addition of more questions

---
+++ practice_questions/questions7_basic.py
@@ -0,0 +1,9 @@
+'''  1. Write a python program to display a user entered 
+name followed by Good Afternoon using input () function.'''
+
+name = input("enter your name sir :-")
+print(f"good afternoon {name}")
+
+# 2. Write a program to detect double space in a string.
+print(name.find("  "))   # -1 Means there is not any double space
+

f2700e06ddcf202d67648e520a80bf5c709a7f77
Yashshakya1 <yashshakya787@gmail.com>
2025-05-05 20:09:23 +0530

    String questions

---
+++ practice_questions/questions6.py
@@ -0,0 +1,19 @@
+# 1. return the lenght of the string 
+name = input('''enter your name :-''')
+print(len(name))
+
+# 2. conver a string in a lower case
+print(name.lower())
+
+# 3. conver a string in a upper case
+capital_name = input("enter your name in small letters :-")
+print(capital_name.upper())
+
+# 4. replace the string into another string
+string = input("enter the name :-")
+print(string.replace(string," noob"))
+
+# 5. split a string into a substring
+a = "Hello, World!"
+b = a.split(",")
+print(b)
